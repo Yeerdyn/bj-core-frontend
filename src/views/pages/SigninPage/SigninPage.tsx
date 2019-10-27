46491e4412af315a8b241c9badbc6b32ac1a591a
Ivan Aldushin <ivan.aldushin@nexthub.ru>
2019-10-27 19:02:00 +0300

    BJ-6 Add form to signin page

--- src/views/pages/SigninPage/SigninPage.tsx
+++ src/views/pages/SigninPage/SigninPage.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import SigninForm from "./partials/SigninForm";
-import bemClassNames from "../../../utilities/bemClassNames";
+import SigninForm from './partials/SigninForm';
+import bemClassNames from '../../../utilities/bemClassNames';
 
 interface ISigninPageProps {}
 
 const b = bemClassNames('signin-page');
 
-const SigninPage: React.FC<ISigninPageProps> = () => <div className={b()}>
-  <SigninForm/>
-</div>;
+const SigninPage: React.FC<ISigninPageProps> = () => (
+  <div className={b()}>
+    <SigninForm />
+  </div>
+);
 
 export default SigninPage;
--- src/views/pages/SigninPage/partials/SigninForm.tsx
+++ src/views/pages/SigninPage/partials/SigninForm.tsx
@@ -1,15 +1,18 @@
-import React, {SyntheticEvent} from "react";
-import {Button, Form, Icon, Input} from "antd";
-import {WrappedFormUtils} from "antd/lib/form/Form";
+import React, { SyntheticEvent } from 'react';
+import { Button, Form, Icon, Input } from 'antd';
+import { WrappedFormUtils } from 'antd/lib/form/Form';
 
 interface ISigninFormProps {
   onSubmit?: (values: object) => void;
-  form: WrappedFormUtils
+  form: WrappedFormUtils;
 }
 
 class SigninForm extends React.Component<ISigninFormProps> {
   handleSubmit = (e: SyntheticEvent) => {
-    const { form: { validateFields }, onSubmit } = this.props;
+    const {
+      form: { validateFields },
+      onSubmit,
+    } = this.props;
 
     e.preventDefault();
 
@@ -21,38 +24,43 @@ class SigninForm extends React.Component<ISigninFormProps> {
   };
 
   render() {
-    const { form: { getFieldDecorator }} = this.props;
+    const {
+      form: { getFieldDecorator },
+    } = this.props;
 
     return (
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item>
-            {getFieldDecorator('username', {
-              rules: [{ required: true, message: 'Please input your username!' }],
-            })(
-              <Input
-                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="Username"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
-            })(
-              <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="Password"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" className="login-form-button">
-              Log in
-            </Button>
-          </Form.Item>
-        </Form>
-    )
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button">
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    );
   }
 }
 
